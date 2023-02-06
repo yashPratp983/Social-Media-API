ef824583876998e987d49db8e50c025a2975e937
yashPratp983 <yash.here07@gmail.com>
2023-02-06 23:45:14 +0530

    added deleating notification route and rectified some bugs

--- routes/notification.js
+++ routes/notification.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { protect, authorisation } = require('../middleware/auth');
-const { createNotification, getNotifications, deleteNotification, acceptNotification } = require('../controller/notification');
+const { createNotification, getNotifications, deleteNotification, acceptNotification, deleteNotification2 } = require('../controller/notification');
 
 router.route('/').post(protect, createNotification).get(protect, getNotifications);
+router.route('/delete/:id').put(protect, deleteNotification2);
 router.route('/accept/:id').put(protect, acceptNotification);
 router.route('/reject/:id').put(protect, deleteNotification);
 
